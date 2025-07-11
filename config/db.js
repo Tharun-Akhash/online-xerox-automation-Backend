ed4bc727757d2908f4ba21fd04fcfa116c114a1c
Tharun-Akhash <tharunakhash2004@gmail.com>
2025-07-12 00:45:25 +0530

    env removed sucess

--- config/db.js
+++ config/db.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/xerox');
-    console.log('MongoDB connected');
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB; // <== THIS IS IMPORTANT
+module.exports = connectDB;
